ef226c38fb758fb11127e7ebd10a5842e8db1bc5
Gichuru Maina <frankgichuru@gmail.com>
2017-12-05 21:44:13 +0300

    Added forms made from the models. work in progress

---
+++ healthixtest/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from . models import Patient, Laboratory_Test, 
+
+class LoginForm(forms.Form):
+	username = forms.CharField(max_length = 100)
+	password = forms.CharField(widget = forms.PasswordInput()) 
+
+class Patient_Form(forms.ModelForm):
+	class Meta:
+		model = Patient
+		fields = ('first_name', 'last_name', 'email', 'mobile', 'date_of_birth', 'id_number')
+
+class Laboratory_Test_Form(forms.Form):
+	class Meta:
+		model = Laboratory_Test
+		fields = ('specimen', 'lab_test', 'duration', 'result', 'date_requested', 'lab', 'doctor', 'patient')
