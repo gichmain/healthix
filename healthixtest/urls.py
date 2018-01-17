773a40b45986eda770ced6ca616d9cb99027066f
Gichuru Maina <frankgichuru@gmail.com>
2018-01-18 00:32:28 +0300

    a little work on using forms derived from classes. next should be usage of said forms to gather data and populate the database

--- healthixtest/urls.py
+++ healthixtest/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from . import views
 
-urlpatterns = [url(r'^$', views.index, name = 'index'), url(r'^view_tests/$', views.view_tests, name = 'view_tests'), url(r'^view_tests/$', views.view_tests, name='view_tests'),
-]
+urlpatterns = [url(r'^$', views.index, name='index'), url(r'^view_tests/$', views.view_tests, name='view_tests'),
+               url(r'^view_tests/$', views.view_tests, name='view_tests'),
+               ]
